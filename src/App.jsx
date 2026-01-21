075ed4a7dcedf766cfa66c4d1eea17eda3fd5fd7
bradgerb <77023448+bradgerb@users.noreply.github.com>
2026-01-20 22:47:23 -0500

    Initial commit and nav bar

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <nav>
+        <div>Home</div>
+        <div>Shop</div>
+        <div>Cart</div>
+      </nav>
+    </>
+  )
+}
+
+export default App
